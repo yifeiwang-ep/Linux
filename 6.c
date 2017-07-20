552e3b74cb8f97b75e6122151060c39ce9547bec
SequinYF <catherine199787@outlook.com>
2017-07-20 10:40:53 +0800

    change

--- 6.c
+++ 6.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <setjmp.h>
+#include <stdlib.h>
 
+static jmp_buf env;
+
+/*
 int main(int argc, char *argv[]){
     int psize = getpagesize();
     printf("%d\n", psize);
     return 0;
+}
+*/
+
+static void dojump(int nvar, int rvar, int vvar){
+    printf("inside dojump(): nvar=%d, rvar=%d, vvar=%d\n", nvar, rvar, vvar);
+    longjmp(env, 1);
+}
+
+
+
+int main(){
+    int nvar;
+    register int rvar;
+    volatile int vvar;
+
+    nvar = 111;
+    rvar = 222;
+    vvar = 333;
+
+    if(setjmp(env) == 0){
+        nvar = 777;
+        rvar = 888;
+        vvar = 999;
+        dojump(nvar, rvar, vvar);
+    }
+    else{
+        printf("after longjmp():nvar=%d, rvar=%d, vvar=%d\n", nvar, rvar, vvar);
+    }
 }
 
